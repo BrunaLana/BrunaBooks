650389e24e4b215047da09d6211d0e9958db630a
Philipe Cabrera <philipegpcabrera@gmail.com>
2025-02-11 06:04:44 +0000

    testar, ajustar e entregar

--- Views/livroCreate.php
+++ Views/livroCreate.php
@@ -17,67 +17,7 @@ $livros = Livro::getAllLivrosAdmin();
 <html>
 <?php include '../Includes/header.php'; ?>
 <link rel="stylesheet" href="../Styles/livroCreate.css">
-<h2 class="titulo">Cadastro de Livro</h2>
-<section class="cadastro_Livros">
-    <form class="form_livro_create" action="../controllers/LivrosController.php" method="POST" enctype="multipart/form-data" style="width: 30%;">
-        <div class="form-group">
-            <label for="title">Título</label>
-            <input type="text" class="form-control texto-campos" id="title" name="titulo" required>
-        </div>
 
-        <div class="form-group">
-            <label for="description">Descrição</label>
-            <textarea id="description" class="form-control" name="descricao" required></textarea>
-        </div>
-
-        <div class="form-group">
-            <label for="price">Preço</label>
-            <input type="number" class="form-control texto-campos" id="price" name="preco" step="0.01" required>
-        </div>
-
-        <div class="form-group">
-            <label for="imagem">Imagem</label>
-            <input type="file" class="form-control texto-campos" id="imagem" name="imagem" accept="image/*" required>
-        </div>
-
-        <div class="form-group">
-            <label for="quantity">Quantidade</label>
-            <input type="number" class="form-control texto-campos" id="quantity" name="quantidade" required>
-        </div>
-        <button type="submit" class="registroBotao btn botao-confirm">Salvar</button>
-    </form>
-</section>
-<section class="listagemLivros container mb-0">
-    <div class="container mt-5">
-        <h2 class="titulo">Listagem de Livros</h2>
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Título</th>
-                    <th>Qtd Estoque</th>
-                    <th>Ativo?</th>
-                    <th>Ações</th>
-                </tr>
-            </thead>
-            <tbody>
-
-                <?php foreach ($livros as $livro): ?>
-                    <tr>
-                        <td><?php echo $livro->productId; ?></td>
-                        <td><?php echo $livro->productName; ?></td>
-                        <td><?php echo $livro->ProductQtt; ?></td>
-                        <td><?php echo $livro->isActive ? 'Sim' : 'Não'; ?></td>
-                        <td>
-                            <a href="?edit_id=<?php echo $livro->productId; ?>" class="btn botao-confirm btn-sm">Editar</a>
-                            <a href="../Controllers/LivrosController.php?action=delete&id=<?php echo $livro->productId; ?>" class="btn botao-delete btn-sm">Excluir</a>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-    </div>
-</section>
 <?php
 if (isset($_GET['edit_id'])) {
     $livroToEdit = Livro::getLivroById($_GET['edit_id']);
@@ -116,10 +56,74 @@ if (isset($_GET['edit_id'])) {
             </div>
 
             <button type="submit" class="registroBotao btn botao-confirm">Salvar</button>
+            <a href="../views/livroCreate.php" class="registroBotao btn botao-delete">Cancelar</a>
 
         </form>
     </section>
+<?php } else {?>
+<h2 class="titulo">Cadastro de Livro</h2>
+<section class="cadastro_Livros">
+    <form class="form_livro_create" action="../controllers/LivrosController.php" method="POST" enctype="multipart/form-data" style="width: 30%;">
+        <div class="form-group">
+            <label for="title">Título</label>
+            <input type="text" class="form-control texto-campos" id="title" name="titulo" required>
+        </div>
+
+        <div class="form-group">
+            <label for="description">Descrição</label>
+            <textarea id="description" class="form-control" name="descricao" required></textarea>
+        </div>
+
+        <div class="form-group">
+            <label for="price">Preço</label>
+            <input type="number" class="form-control texto-campos" id="price" name="preco" step="0.01" required>
+        </div>
+
+        <div class="form-group">
+            <label for="imagem">Imagem</label>
+            <input type="file" class="form-control texto-campos" id="imagem" name="imagem" accept="image/*" required>
+        </div>
+
+        <div class="form-group">
+            <label for="quantity">Quantidade</label>
+            <input type="number" class="form-control texto-campos" id="quantity" name="quantidade" required>
+        </div>
+        <button type="submit" class="registroBotao btn botao-confirm">Salvar</button>
+    </form>
+</section>
 <?php } ?>
+<section class="listagemLivros container mb-0">
+    <div class="container mt-5">
+        <h2 class="titulo">Listagem de Livros</h2>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Título</th>
+                    <th>Qtd Estoque</th>
+                    <th>Ativo?</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <?php foreach ($livros as $livro): ?>
+                    <tr>
+                        <td><?php echo $livro->productId; ?></td>
+                        <td><?php echo $livro->productName; ?></td>
+                        <td><?php echo $livro->ProductQtt; ?></td>
+                        <td><?php echo $livro->isActive ? 'Sim' : 'Não'; ?></td>
+                        <td>
+                            <a href="?edit_id=<?php echo $livro->productId; ?>" class="btn botao-confirm btn-sm">Editar</a>
+                            <a href="../Controllers/LivrosController.php?action=delete&id=<?php echo $livro->productId; ?>" class="btn botao-delete btn-sm">Excluir</a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+</section>
+
 <?php include '../Includes/contatoFooter.php'; ?>
 
 </html>
